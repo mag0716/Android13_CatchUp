879db791f66b7785e9024902982b1f24bd3301ac
mag0716 <mkishimoto0716@gmail.com>
2022-03-21 21:11:21 +0900

    ACTIO_PICK_IMAGESで画像選択する処理を実装

--- samples/PhotoPickerSample/app/src/main/java/com/github/mag0716/photopickersample/MainActivity.kt
+++ samples/PhotoPickerSample/app/src/main/java/com/github/mag0716/photopickersample/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.github.mag0716.photopickersample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var pickImageLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            Log.d("xxx", "pick image : ${data?.data}")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.pick_image_button).setOnClickListener {
+            pickImage()
+        }
+    }
+
+    private fun pickImage() {
+        pickImageLauncher.launch(
+            Intent(MediaStore.ACTION_PICK_IMAGES).apply {
+                //putExtra(MediaStore.EXTRA_PICK_IMAGES_MAX, 1)
+            }
+        )
     }
 }
