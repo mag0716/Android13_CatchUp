b823309e50f4413cfc051adeef8d2a0b04365133
mag0716 <mkishimoto0716@gmail.com>
2022-03-24 00:08:13 +0900

    FGS Task Manager確認のためのForeground Serviceを起動、停止するコードを実装

--- samples/ForegroundServiceTaskManagerSample/app/src/main/java/com/github/mag0716/foregroundservicetaskmanagersample/MainActivity.kt
+++ samples/ForegroundServiceTaskManagerSample/app/src/main/java/com/github/mag0716/foregroundservicetaskmanagersample/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.github.mag0716.foregroundservicetaskmanagersample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.start_button).setOnClickListener {
+            val intent = Intent(this, LoggingForegroundService::class.java)
+            startService(intent)
+        }
+
+        findViewById<Button>(R.id.stop_button).setOnClickListener {
+            val intent = Intent(this, LoggingForegroundService::class.java)
+            stopService(intent)
+        }
     }
 }
